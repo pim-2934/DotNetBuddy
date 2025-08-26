0153bc5b088ff3a1244c223792877b698544a05f
Pim <pim.reijersen@gmail.com>
2025-08-26 09:45:02 +0200

    Rename `WebApplicationExtensions` to `ApplicationBuilderExtensions` and update `UseBuddyExceptions` to use `IApplicationBuilder`.

--- DotNetBuddy.Presentation/Extensions/WebApplicationExtensions.cs
+++ DotNetBuddy.Presentation/Extensions/WebApplicationExtensions.cs
@@ -6,13 +6,13 @@ namespace DotNetBuddy.Presentation.Extensions;
 /// <summary>
 /// Provides extension methods for the WebApplication class, enabling additional functionalities.
 /// </summary>
-public static class WebApplicationExtensions
+public static class ApplicationBuilderExtensions
 {
     /// <summary>
     /// Configures the application to use exception handlers using BuddyExceptionMiddleware.
     /// </summary>
     /// <param name="app">The WebApplication instance this method extends.</param>
-    public static void UseBuddyExceptions(this WebApplication app)
+    public static void UseBuddyExceptions(this IApplicationBuilder app)
     {
         app.UseMiddleware<ExceptionMiddleware>();
     }
