8f43b1253d87edb09bff82a44e191324ad87d90b
Pim <pim.reijersen@gmail.com>
2025-09-02 10:44:41 +0200

    Add `ValidationException` for standardized validation error handling
    - Introduced `ValidationException` in `DotNetBuddy.Domain.Exceptions` to streamline error reporting for validation failures.
    - Updated `ValidateInterceptor` to utilize `ValidationException` for enhanced clarity and consistency.

---
+++ DotNetBuddy.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetBuddy.Domain.Exceptions;
+
+/// <summary>
+/// Represents an exception thrown when validation of an entity or object fails.
+/// </summary>
+/// <remarks>
+/// This exception is specifically used to handle and convey validation errors encountered during operations such as data persistence or object validation.
+/// It provides a default message indicating that one or more validation errors have occurred and includes specific details of these errors.
+/// The exception conforms to the RFC 9110 standards through its inheritance of the <see cref="Rfc9110Exception"/> class.
+/// </remarks>
+/// <seealso cref="Rfc9110Exception"/>
+public class ValidationException(string detail, int statusCode = 400)
+    : Rfc9110Exception("One or more validation errors occurred.", detail, statusCode);
