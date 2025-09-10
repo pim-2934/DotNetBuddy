6a5befa9220fa9c0bce39ca5b6fd566e6111b709
Pim <pim.reijersen@gmail.com>
2025-09-10 13:46:31 +0200

    Remove redundant `<typeparam>` documentation from `IValidator` interface.

--- DotNetBuddy.Domain/IValidator.cs
+++ DotNetBuddy.Domain/IValidator.cs
@@ -15,8 +15,6 @@ public interface IValidator<in TSource, in TInput> where TSource : class where T
     /// <param name="source">The source object to be validated.</param>
     /// <param name="input">The input object containing the validation criteria.</param>
     /// <param name="cancellationToken">An optional token to observe for cancellation requests.</param>
-    /// <typeparam name="TSource">The type of the source object to validate, must be a reference type.</typeparam>
-    /// <typeparam name="TInput">The type of the input object used for validation, must be a reference type.</typeparam>
     /// <returns>A collection of <see cref="ValidationResult"/> objects that represent the results of validation.
     /// If no validator is registered, or if no validation errors occur, an empty collection is returned.</returns>
     IAsyncEnumerable<ValidationResult> ValidateAsync(TSource source, TInput input,
